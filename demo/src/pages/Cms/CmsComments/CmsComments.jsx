a53ab13448ed5953095e9de749e5bc4d34333998
masproo22 <m.abbaszadeh2002@gmail.com>
2024-01-17 23:18:30 +0330

    fix comment

--- demo/src/pages/Cms/CmsComments/CmsComments.jsx
+++ demo/src/pages/Cms/CmsComments/CmsComments.jsx
@@ -21,28 +21,21 @@ export default function CmsComments() {
 
   return (
     <div className="CmsComments">
-      {comments.length ?
+      {comments.length ? (
         comments.map((comment) => (
           <div key={comment.id} className="comment">
             <img src="../../../../public/images/comment.svg" />
             <div className="CmsComments__details">
               <p className="asdfadf">subject</p>
-              <p className="rtyqqf">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
-                ut blanditiis provident sed vel repudiandae, voluptatum porro in
-                molestias rerum odit modi beatae veniam expedita placeat
-                assumenda voluptas, iure maiores. Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Ipsa cumque eius vero reiciendis
-                velit dolore quas. Atque, vel. Corrupti, perspiciatis!
-              </p>
+              <p className="rtyqqf">{comment.massage}</p>
               <div className="trawert">
                 <div className="stuyqr">
                   <p>
                     {comment.firstName} {comment.lastName}
                   </p>
 
-                  <p>safhgd@gmail.com</p>
-                  <p>0934561434123</p>
+                  <p>{comment.email}</p>
+                  <p>{comment.phone}</p>
                 </div>
                 <div
                   onClick={() => deleteCommentHandler(comment.id)}
@@ -51,10 +44,17 @@ export default function CmsComments() {
               </div>
             </div>
           </div>
-        )): (<div className="noComments">
-            <img className="image noComments__image" src="../../../../public/images/sad.svg" alt="unavailable-image" />
-            <p className="noComments__text">No Comment Here</p>
-        </div>)}
+        ))
+      ) : (
+        <div className="noComments">
+          <img
+            className="image noComments__image"
+            src="../../../../public/images/sad.svg"
+            alt="unavailable-image"
+          />
+          <p className="noComments__text">No Comment Here</p>
+        </div>
+      )}
     </div>
   );
 }
